38ea7261c4ae3b9f3317e883f4ccf96abb5218e8
datquangtran <tranquangdat1996@gmail.com>
2020-04-17 21:05:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,265 +1,296 @@
-$(function()
-{
-    var playerTrack = $("#player-track");
-	var bgArtwork = $('#bg-artwork');
-	var bgArtworkUrl;
-	var albumName = $('#album-name');
-	var trackName = $('#track-name');
-	var albumArt = $('#album-art'),
-		sArea = $('#s-area'),
-		seekBar = $('#seek-bar'),
-		trackTime = $('#track-time'),
-		insTime = $('#ins-time'),
-		sHover = $('#s-hover'),
-		playPauseButton = $("#play-pause-button"),
-		i = playPauseButton.find('i'),
-		tProgress = $('#current-time'),
-		tTime = $('#track-length'),
-		seekT, seekLoc, seekBarPos, cM, ctMinutes, ctSeconds, curMinutes, curSeconds, durMinutes, durSeconds, playProgress, bTime, nTime = 0,
-		buffInterval = null, tFlag = false;
-	
-	var playPreviousTrackButton = $('#play-previous'), playNextTrackButton = $('#play-next'), currIndex = -1;
-	
-	var songs = [{
-		artist: "Dig Didzay",
-		name: "Nếu Anh Đi (Cover)",
-		url: "Musics/NeuAnhDi.mp3",
-		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
-	}];
-	
-	function shuffle(a) {
-		var j, x, i;
-		for (i = a.length - 1; i > 0; i--) {
-			j = Math.floor(Math.random() * (i + 1));
-			x = a[i];
-			a[i] = a[j];
-			a[j] = x;
-		}
-		return a;
-	}
-	songs = shuffle(songs);
+const apiKeyList = ["AIzaSyAKkNJJh2kbSgl31RObQuuEaS_6oRzT30Q", "AIzaSyAvPUsjjqCxjx9ZlIZh-EcdiBAFbJOeoO0", "AIzaSyB56E3cgBh0TMpNi5WQJT9AMFtChFIeEIo"];
+var apiKey = apiKeyList[0];
+var listVid = [];
+var listVideo;
+var player;
 
-    function playPause()
-    {
-        setTimeout(function()
-        {
-            if(audio.paused)
-            {
-                playerTrack.addClass('active');
-                albumArt.addClass('active');
-                checkBuffering();
-                i.attr('class','fas fa-pause');
-                audio.play();
-            }
-            else
-            {
-                playerTrack.removeClass('active');
-                albumArt.removeClass('active');
-                clearInterval(buffInterval);
-                albumArt.removeClass('buffering');
-                i.attr('class','fas fa-play');
-                audio.pause();
-            }
-        },300);
-    }
+var bg = document.getElementsByClassName('bg')[0];
+var musicPlayer = document.getElementsByClassName('player')[0];
+var prev = document.getElementsByClassName('btn-prev')[0];
+var next = document.getElementsByClassName('btn-next')[0];
+var repeat = document.getElementsByClassName('btn-repeat')[0];
+var form = document.getElementsByClassName('form')[0];
+var newPlaylistId = document.getElementsByClassName('input')[0];
+var ok = document.getElementsByClassName('ok')[0];
+var body = document.getElementsByTagName('body')[0];
 
-    	
-	function showHover(event)
-	{
-		seekBarPos = sArea.offset(); 
-		seekT = event.clientX - seekBarPos.left;
-		seekLoc = audio.duration * (seekT / sArea.outerWidth());
-		
-		sHover.width(seekT);
-		
-		cM = seekLoc / 60;
-		
-		ctMinutes = Math.floor(cM);
-		ctSeconds = Math.floor(seekLoc - ctMinutes * 60);
-		
-		if( (ctMinutes < 0) || (ctSeconds < 0) )
-			return;
-		
-        if( (ctMinutes < 0) || (ctSeconds < 0) )
-			return;
-		
-		if(ctMinutes < 10)
-			ctMinutes = '0'+ctMinutes;
-		if(ctSeconds < 10)
-			ctSeconds = '0'+ctSeconds;
-        
-        if( isNaN(ctMinutes) || isNaN(ctSeconds) )
-            insTime.text('--:--');
-        else
-		    insTime.text(ctMinutes+':'+ctSeconds);
-            
-		insTime.css({'left':seekT,'margin-left':'-21px'}).fadeIn(0);
-		
-	}
+var tag = document.createElement('script');
+var btn = document.getElementById('btn');
+var btn2 = document.getElementById('btn2');
+var icon = document.getElementById('icon');
+var icon2 = document.getElementById('icon2');
+var para = document.getElementById('title');
 
-    function hideHover()
-	{
-        sHover.width(0);
-        insTime.text('00:00').css({'left':'0px','margin-left':'0px'}).fadeOut(0);		
-    }
-    
-    function playFromClickedPos()
-    {
-        audio.currentTime = seekLoc;
-		seekBar.width(seekT);
-		hideHover();
-    }
+var rand;
+var repeatStatus = 0;
 
-    function updateCurrTime()
-	{
-        nTime = new Date();
-        nTime = nTime.getTime();
+//Request de lay Playlist Item
+const getPlayListItems = async playlistID => {
+	var token;
+	var resultArr = [];
+    const result = await axios.get(`https://www.googleapis.com/youtube/v3/playlistItems`, {
+      params: {
+        part: 'id,snippet',
+        maxResults: 50,
+        playlistId: playlistID,
+        key: apiKey
+      }
+    })
+    //Lay NextPage Token
+	token = result.data.nextPageToken;
+	resultArr.push(result.data);
+	while (token) {
+		let result = await axios.get(`https://www.googleapis.com/youtube/v3/playlistItems`, {
+      	params: {
+        part: 'id,snippet',
+        maxResults: 50,
+        playlistId: playlistID,
+        key: apiKey,
+		pageToken: token
+      	}
+    	})
+		token = result.data.nextPageToken;
+		resultArr.push(result.data);
+	}	
+	return resultArr;
+};
 
-        if( !tFlag )
-        {
-            tFlag = true;
-            trackTime.addClass('active');
-        }
-
-		curMinutes = Math.floor(audio.currentTime / 60);
-		curSeconds = Math.floor(audio.currentTime - curMinutes * 60);
-		
-		durMinutes = Math.floor(audio.duration / 60);
-		durSeconds = Math.floor(audio.duration - durMinutes * 60);
-		
-		playProgress = (audio.currentTime / audio.duration) * 100;
-		
-		if(curMinutes < 10)
-			curMinutes = '0'+curMinutes;
-		if(curSeconds < 10)
-			curSeconds = '0'+curSeconds;
-		
-		if(durMinutes < 10)
-			durMinutes = '0'+durMinutes;
-		if(durSeconds < 10)
-			durSeconds = '0'+durSeconds;
-        
-        if( isNaN(curMinutes) || isNaN(curSeconds) )
-            tProgress.text('00:00');
-        else
-		    tProgress.text(curMinutes+':'+curSeconds);
-        
-        if( isNaN(durMinutes) || isNaN(durSeconds) )
-            tTime.text('00:00');
-        else
-		    tTime.text(durMinutes+':'+durSeconds);
-        
-        if( isNaN(curMinutes) || isNaN(curSeconds) || isNaN(durMinutes) || isNaN(durSeconds) )
-            trackTime.removeClass('active');
-        else
-            trackTime.addClass('active');
-
-        
-		seekBar.width(playProgress+'%');
-		
-		if( playProgress == 100 )
-		{
-			i.attr('class','fa fa-play');
-			seekBar.width(0);
-            tProgress.text('00:00');
-            albumArt.removeClass('buffering').removeClass('active');
-            clearInterval(buffInterval);
-			selectTrack(1);
-		}
-    }
-    
-    function checkBuffering()
-    {
-        clearInterval(buffInterval);
-        buffInterval = setInterval(function()
-        { 
-            if( (nTime == 0) || (bTime - nTime) > 1000  )
-                albumArt.addClass('buffering');
-            else
-                albumArt.removeClass('buffering');
-
-            bTime = new Date();
-            bTime = bTime.getTime();
-
-        },100);
-    }
-
-    function selectTrack(flag)
-    {
-        if( flag == 0 || flag == 1 )
-            ++currIndex;
-        else
-            --currIndex;
-
-        if( (currIndex > -1) && (currIndex < songs.length) )
-        {
-            if( flag == 0 )
-                i.attr('class','fa fa-play');
-            else
-            {
-                albumArt.removeClass('buffering');
-                i.attr('class','fa fa-pause');
-            }
-
-            seekBar.width(0);
-            trackTime.removeClass('active');
-            tProgress.text('00:00');
-            tTime.text('00:00');
-			
-			currAlbum = songs[currIndex].name;
-            currTrackName = songs[currIndex].artist;
-            currArtwork = songs[currIndex].picture;
-
-            audio.src = songs[currIndex].url;
-            
-            nTime = 0;
-            bTime = new Date();
-            bTime = bTime.getTime();
-
-            if(flag != 0)
-            {
-                audio.play();
-                playerTrack.addClass('active');
-                albumArt.addClass('active');
-            
-                clearInterval(buffInterval);
-                checkBuffering();
-            }
-
-            albumName.text(currAlbum);
-            trackName.text(currTrackName);
-            $('#album-art img').prop('src', bgArtworkUrl);
-        }
-        else
-        {
-            if( flag == 0 || flag == 1 )
-                --currIndex;
-            else
-                ++currIndex;
-        }
-    }
-
-    function initPlayer()
-	{	
-        audio = new Audio();
-
-		selectTrack(0);
-		
-		audio.loop = false;
-		
-		playPauseButton.on('click',playPause);
-		
-		sArea.mousemove(function(event){ showHover(event); });
-		
-        sArea.mouseout(hideHover);
-        
-        sArea.on('click',playFromClickedPos);
-		
-        $(audio).on('timeupdate',updateCurrTime);
-
-        playPreviousTrackButton.on('click',function(){ selectTrack(-1);} );
-        playNextTrackButton.on('click',function(){ selectTrack(1);});
-	}
-    
-	initPlayer();
+//Xu li Item de lay Title video va videoId
+getPlayListItems("UUWovDkXc7w0821EaKmznmCA")
+.then(data => {
+	data.forEach(item => {
+    	item.items.forEach(i => listVid.push({title: i.snippet.title, idVid: i.snippet.resourceId.videoId}));
+	});
+	//Tao random index
+    rand = Math.floor(Math.random()*listVid.length);
+    checkPrivate();
+    tag.src = "https://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+})
+.catch(err => {
+	changeAPIKey(apiKeyList[1], err);
 });
+
+
+function changeAPIKey(newKey, err) {
+	if (err.response.data.error.errors[0].reason == "dailyLimitExceeded") {
+		apiKey = newKey;
+		getPlayListItems("UUWovDkXc7w0821EaKmznmCA")
+		.then(data => {
+			data.forEach(item => {
+	    	item.items.forEach(i => listVid.push({title: i.snippet.title, idVid: i.snippet.resourceId.videoId}));
+			
+			rand = Math.floor(Math.random()*listVid.length);
+		    checkPrivate();
+		    tag.src = "https://www.youtube.com/iframe_api";
+			var firstScriptTag = document.getElementsByTagName('script')[0];
+			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+		});
+		})
+		.catch(err => {
+			changeAPIKey(apiKeyList[2], err);
+		});
+
+		
+	}
+}
+
+
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('player', {
+	  height: '0',
+	  width: '0',
+	  videoId: listVid[rand].idVid,
+	  events: {
+	    'onReady': onPlayerReady,
+	    'onStateChange': onPlayerStateChange
+	  }
+	});
+}
+
+function onPlayerReady(event) {
+	player.setPlaybackQuality("small");
+	btn.style.display = "block";
+	prev.style.display = "block";
+	next.style.display = "block";
+	btn2.style.display = "block";
+	repeat.style.display = "block";
+	form.style.display = "flex";
+	para.innerHTML = listVid[rand].title;
+	playButton(player.getPlayerState() !== 5);
+}
+
+//On click button
+btn.onclick = changeStatusPlay;
+prev.onclick = prevSong;
+next.onclick = nextSong;
+repeat.onclick = repeatVideo;
+ok.onclick = changePlaylistId;
+
+function playButton(play) {
+		icon.src = play ? "icon/pause.svg" : "icon/play.svg";
+}
+
+function changeStatusPlay() {
+	if (player.getPlayerState() == 1 || player.getPlayerState() == 3) {
+		pauseVideo();
+		playButton(false);
+	} else if (player.getPlayerState() != 0) {
+		playVideo();
+    	playButton(true);
+    	
+    } 
+}
+
+function onPlayerStateChange(event) {
+    if (event.data === 0) {
+    	playButton(false); 
+    }
+}
+
+function playVideo() {
+	player.playVideo();
+}
+
+function pauseVideo() {
+	player.pauseVideo();
+}
+
+function stopVideo() {
+	player.stopVideo();
+}
+
+//previous song
+function prevSong() {
+	if (repeatStatus == 1) {
+		repeat.style.opacity = "0.3";
+		repeatStatus = 0;
+	}
+	playButton(false);
+	stopVideo();
+	if (rand - 1 < 0) {
+		rand = listVid.length - 1;
+	} else {
+		rand -= 1;
+	}
+	checkPrivateBack();
+	player.loadVideoById({videoId:listVid[rand].idVid});
+	para.innerHTML = listVid[rand].title;
+	musicPlayer.style.backgroundImage = "url('https://source.unsplash.com/random/600*250/?landscape')";
+	bg.style.backgroundImage = "url('https://source.unsplash.com/random/600*250/?landscape')";
+	playButton(true);			
+}
+
+//next song
+function nextSong() {
+	if (repeatStatus == 1) {
+		repeat.style.opacity = "0.3";
+		repeatStatus = 0;
+	}
+	playButton(false);
+	stopVideo();
+	if (rand + 1 == listVid.length) {
+		rand = 0;
+	} else {
+		rand += 1;
+	}
+	checkPrivate();
+	player.loadVideoById({videoId:listVid[rand].idVid});
+	para.innerHTML = listVid[rand].title;
+	musicPlayer.style.backgroundImage = "url('https://source.unsplash.com/random/600*250/?landscape')";
+	bg.style.backgroundImage = "url('https://source.unsplash.com/random/600*250/?landscape')";
+	playButton(true);
+
+}
+
+//Next bai moi khi video het
+function nextVideo() {
+	if (repeatStatus == 1) {
+		player.loadVideoById({videoId:listVid[rand].idVid});
+	} else {
+		rand = Math.round(Math.random()*listVid.length);
+		checkPrivate();
+		player.loadVideoById({videoId:listVid[rand].idVid});
+		para.innerHTML = listVid[rand].title;
+		musicPlayer.style.backgroundImage = "url('https://source.unsplash.com/random/600*250/?landscape')";
+		bg.style.backgroundImage = "url('https://source.unsplash.com/random/600*250/?landscape')";
+	}
+	
+}
+
+//Phat lap lai
+function repeatVideo () {
+	if (repeatStatus == 0) {
+		repeat.style.opacity = "0.8";
+		repeatStatus = 1;
+	} else {
+		repeat.style.opacity = "0.3";
+		repeatStatus = 0;
+	}
+}
+
+//Kiem tra co phai video private hoac delete khong
+function checkPrivate() {
+	if (listVid[rand].title == "Private video" || listVid[rand].title == "Deleted video") {
+		if (rand == listVid.length - 1) {
+			rand = 0;
+		} else {
+			rand += 1;
+		}
+		checkPrivate();
+	}
+};
+
+function checkPrivateBack() {
+	if (listVid[rand].title == "Private video" || listVid[rand].title == "Deleted video") {
+		if (rand == 0) {
+			rand = listVid.length - 1;
+		} else {
+			rand -= 1;
+		}		
+		checkPrivateBack();
+	}
+};
+
+//Khi nhap Playlist Id moi
+function changePlaylistId () {
+	var newId = newPlaylistId.value;
+	if (newId == "") {
+		return;
+	}
+
+	listVid = [];
+	btn.style.display = "none";
+	prev.style.display = "none";
+	next.style.display = "none";
+	btn2.style.display = "none";
+	repeat.style.display = "none";
+	para.innerHTML = "Loading...";
+
+	getPlayListItems(newId)
+	.then(data => {
+		data.forEach(item => {
+	    	item.items.forEach(i => listVid.push({title: i.snippet.title, idVid: i.snippet.resourceId.videoId}));
+		});
+	    rand = Math.floor(Math.random()*listVid.length);
+	    checkPrivate();
+	    btn.style.display = "block";
+		prev.style.display = "block";
+		next.style.display = "block";
+		btn2.style.display = "block";
+		repeat.style.display = "block";
+		para.innerHTML = listVid[rand].title;			    
+		player.loadVideoById({videoId:listVid[rand].idVid});
+		playButton(true);
+	});	
+
+}
+
+//Chuyen bai
+setInterval(function() {
+	if (player.getPlayerState() == 0) {
+		nextVideo();
+		playButton(true);
+	}
+}, 3000);
